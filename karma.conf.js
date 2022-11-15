50062f6c8b4dbe7e53f6b2d0d5a780a477dee2a4
Vitor Lopes <vitorlopesfranchi@gmail.com>
2022-11-15 09:19:20 -0300

    new version

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/healthLedger2'),
+      dir: require('path').join(__dirname, './coverage/health-ledger2'),
       subdir: '.',
       reporters: [
         { type: 'html' },
