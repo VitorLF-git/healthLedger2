4b16ce5f7674cf7860d01aed5a4f3232c6e559b8
Vitor Lopes <vitorlopesfranchi@gmail.com>
2022-11-15 13:50:16 -0300

    add tabela labs e dash

--- src/app/components/labs/labs.component.ts
+++ src/app/components/labs/labs.component.ts
@@ -3,21 +3,20 @@ import { Component, OnInit } from '@angular/core';
 export interface PeriodicElement {
   nome: string;
   position: number;
-  quantidade: number;
-  hospital: string;
+  endereco: string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, nome: 'Medicamento 1', quantidade: 152, hospital: 'Hcor'},
-  {position: 2, nome: 'Medicamento 2', quantidade: 20, hospital: 'Hcor'},
-  {position: 3, nome: 'Medicamento 3', quantidade: 20, hospital: 'Hcor'},
-  {position: 4, nome: 'Medicamento 4', quantidade: 20, hospital: 'Hcor'},
-  {position: 5, nome: 'Medicamento 5', quantidade: 20, hospital: 'Hcor'},
-  {position: 6, nome: 'Medicamento 6', quantidade: 20, hospital: 'Hcor'},
-  {position: 7, nome: 'Medicamento 7', quantidade: 20, hospital: 'Hcor'},
-  {position: 8, nome: 'Medicamento 8', quantidade: 20, hospital: 'Hcor'},
-  {position: 9, nome: 'Medicamento 9', quantidade: 20, hospital: 'Hcor'},
-  {position: 10, nome: 'Medicamento 10', quantidade: 20, hospital: 'Hcor'},
+  {position: 1, nome: 'Laboratório 1', endereco: 'rua 123, sao paulo, sp'},
+  {position: 2, nome: 'Laboratório 2', endereco: 'rua 123, sao paulo, sp'},
+  {position: 3, nome: 'Laboratório 3', endereco: 'rua 123, sao paulo, sp'},
+  {position: 4, nome: 'Laboratório 4', endereco: 'rua 123, sao paulo, sp'},
+  {position: 5, nome: 'Laboratório 5', endereco: 'rua 123, sao paulo, sp'},
+  {position: 6, nome: 'Laboratório 6', endereco: 'rua 123, sao paulo, sp'},
+  {position: 7, nome: 'Laboratório 7', endereco: 'rua 123, sao paulo, sp'},
+  {position: 8, nome: 'Laboratório 8', endereco: 'rua 123, sao paulo, sp'},
+  {position: 9, nome: 'Laboratório 9', endereco: 'rua 123, sao paulo, sp'},
+  {position: 10, nome: 'Laboratório 10', endereco: 'rua 123, sao paulo, sp'},
 ];
 
 @Component({
@@ -29,7 +28,7 @@ export class LabsComponent implements OnInit {
 
   constructor() { }
 
-  displayedColumns: string[] = ['position', 'nome', 'quantidade', 'hospital'];
+  displayedColumns: string[] = ['position', 'nome', 'endereco', 'actions'];
   dataSource = ELEMENT_DATA;
   
   ngOnInit(): void {
